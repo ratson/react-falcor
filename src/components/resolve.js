f2b28eaea493e16354a97fe4e3f41484848580ff
Ratson <github@ratson.name>
2016-08-20 23:08:20 +0800

    Implement resolve() with Resolve

--- src/components/resolve.js
+++ src/components/resolve.js
@@ -1,19 +1,75 @@
-import {AsyncState as asyncState} from 'react-async-state'
-import compose from 'recompose/compose'
-import omit from 'lodash/omit'
+import _ from 'lodash'
 
-import withFalcor from './withFalcor'
+import React, {PropTypes} from 'react'
+import hoistStatics from 'hoist-non-react-statics'
 
-export default (mapFalcorToProps) => compose(
-  withFalcor(({falcor}) => ({
-    __v: falcor.getVersion(),
-  })),
-  asyncState((ownProps) => {
-    const {falcor} = ownProps
-    return mapFalcorToProps(falcor, ownProps)
-  }, {
-    mergeProps(props, state) {
-      return Object.assign(omit(props, '__v'), state)
-    },
-  }),
-)
+function mergeProps(props, nextProps) {
+  return {
+    ...props,
+    ...nextProps,
+  }
+}
+
+function getInitialState() {
+  return {
+    version: 0,
+    nextProps: {},
+  }
+}
+
+export default (mapFalcorToProps, {pure = true} = {}) => {
+  return (WrappedComponent) => {
+    class Resolve extends React.Component {
+      constructor(props, context) {
+        super(props, context)
+
+        this.falcor = props.falcor || context.falcor
+        this.state = getInitialState(props)
+
+        this._counter = 0
+        this._cancelPromise = false
+      }
+
+      componentDidMount() {
+        this.mapAsyncDataToProps()
+      }
+
+      shouldComponentUpdate(nextProps, nextState) {
+        return !pure || !_.isEqual(this.props, nextProps) || !_.isEqual(this.state, nextState)
+      }
+
+      componentWillUnmount() {
+        this._cancelPromise = true
+      }
+
+      mapAsyncDataToProps(props = this.props) {
+        const counter = ++this._counter
+
+        Promise.resolve(mapFalcorToProps(this.falcor, props)).then((nextProps) => {
+          // resolve to nothing when cancelled or has pending resolve
+          if (this._cancelPromise || counter !== this._counter) {
+            return
+          }
+
+          this.setState({
+            version: this.falcor.getVersion(),
+            nextProps,
+          })
+        })
+      }
+
+      render() {
+        return <WrappedComponent {...mergeProps(this.props, this.state.nextProps)}/>
+      }
+    }
+
+    Resolve.contextTypes = {
+      falcor: PropTypes.object,
+    }
+    Resolve.propTypes = {
+      falcor: PropTypes.object,
+    }
+
+    return hoistStatics(Resolve, WrappedComponent)
+  }
+}
