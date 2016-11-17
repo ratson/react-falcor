9dce874044f9aa10bbaef45b081e71d207a265cd
Ratson <github@ratson.name>
2016-11-17 23:20:16 +0800

    Assert Falcor model properties

--- test/fixtures/model.spec.js
+++ test/fixtures/model.spec.js
@@ -1,9 +1,17 @@
-import 'should'
+import should from 'should'
 
 import model from './model'
 
 describe('model', () => {
   it('can read greeting message', () => {
     return model.getValue('greeting').should.be.finally.equal('Hello World!')
+  })
+
+  it('has _root.onChange()', () => {
+    return should(model._root.onChange).be.a.Function()
+  })
+
+  it('has _request.requests array', () => {
+    return should(model._request.requests).be.a.Array()
   })
 })
